9072ac44b68370470dc7672706fc9cd972e2987b
ArtemNozhenko <artem2516nozh@gmail.com>
2024-01-19 00:39:01 +0100

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,13 @@
-function makeTransaction(
-  quantity,
-  pricePerDroid
-) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalCredits = quantity * pricePerDroid;
+  if (quantity * pricePerDroid <= customerCredits) {
+    return `You ordered ${quantity} droids worth ${totalCredits} credits!`;
+  } else {
+    return `Insufficient funds!`;
+  }
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
